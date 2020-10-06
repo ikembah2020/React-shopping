d07d652218e0025bd3ff32e3b0be20cfc1eaacaf
Merriman-Sly <ikembah2020@gmail.com>
2020-10-06 03:26:28 +0100

    created react app and added the homepage route

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import HomePage from "./Homepage.jsx";
+
+class App extends Component {
+  render() {
+    return <div>
+      <HomePage />
+    </div>;
+  }
+}
+
+export default App;
