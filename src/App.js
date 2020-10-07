6d013aa0cb1bfd847d990fdb9d40cb892769264c
Merriman-Sly <ikembah2020@gmail.com>
2020-10-07 20:40:43 +0100

    Added collectionpreview/item, customButton, signin form and installed firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.jsx';
+
 import './App.css';
 
+import SignInUp from './pages/sign-in-up/sign-in-up.jsx';
 
 import HomePage from "./pages/homepage/Homepage.jsx";
+import ShopPage from "./pages/shop/shop.jsx";
 
 class App extends Component {
   render() {
     return <div>
+      <Header />
       <div className='headText'>
       <h1>MERRY COMPLEX</h1>
       </div>
+
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUp} />
+      </Switch>
+    </div>
       
-      <HomePage />
     </div>;
   }
 }
